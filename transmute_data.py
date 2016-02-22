5f894f4431a996b8b3f67353e2bd715693f3ea4b
Anthony Scopatz <scopatz@gmail.com>
2016-02-22 16:55:44 -0500

    raw nuclear data

---
+++ transmute_data.py
@@ -0,0 +1,54 @@
+import json
+
+import numpy as np
+
+from pyne import nucname
+from pyne import data
+from pyne import rxname
+
+DECAY_RXS = ['bminus', 'bplus', 'ec', 'alpha', 'it', 'sf', 'bminus_n']
+XS_RXS = ['gamma', 'z_2n', 'z_3n', 'alpha', 'fission', 'proton', 'gamma_1', 
+          'z_2n_1']
+
+
+def add_child_decays(nuc, symbols):
+    childname = nucname.name(nuc)
+    for rx in DECAY_RXS:
+        try:
+            parent = rxname.parent(nuc, rx, b'decay')
+        except RuntimeError:
+            continue
+        if data.branch_ratio(parent, nuc) < 1e-16:
+            continue
+        parname = nucname.name(parent)
+        symbols['lambda_' + parname] = data.decay_const(parent)
+        gamma = 'gamma_{0}_{1}_{2}'.format(parname, childname, rx)
+        symbols[gamma] = data.branch_ratio(parent, nuc)
+
+
+def main():
+    # get list of nuclides
+    data.atomic_mass('U235')
+    nucs = set(data.atomic_mass_map.keys())
+    for nuc in data.atomic_mass_map:
+        nucm = nuc + 1
+        if nucname.anum(nuc) == 0 or data.decay_const(nucm) < 1e-16 or \
+                            data.decay_const(nuc) == data.decay_const(nucm):
+            continue
+        nucs.add(nucm)
+    nucs = [nuc for nuc in nucs if nucname.anum(nuc) > 0 and
+                                not np.isnan(data.decay_const(nuc)) and
+                                nuc < 200000000]
+    nucs.sort()
+    # get symbols
+    symbols = {}
+    for nuc in nucs:
+        add_child_decays(nuc, symbols)
+    # print symbols
+    d = {'symbols': symbols, 'nucs': list(map(nucname.name, nucs))}
+    s = json.dumps(d, indent=4, sort_keys=True)
+    print(s)
+
+
+if __name__ == '__main__':
+    main()
